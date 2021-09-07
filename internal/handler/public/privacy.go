1967cb08bd8b84b92b9dde00d1bbfba7197363d7
Nathan Hollows <me@nathanhollows.com>
2021-09-07 23:43:56 +1200

    Privacy page is now editable

--- internal/handler/public/privacy.go
+++ internal/handler/public/privacy.go
@@ -5,13 +5,16 @@ import (
 
 	"github.com/nathanhollows/Argon/internal/flash"
 	"github.com/nathanhollows/Argon/internal/handler"
+	"github.com/nathanhollows/Argon/internal/models"
 )
 
 // Privacy tells visitors about the project terms and privacy conditions
 func Privacy(env *handler.Env, w http.ResponseWriter, r *http.Request) error {
 	w.Header().Set("Content-Type", "text/html")
 	data := make(map[string]interface{})
+	page := models.Page{}
+	env.DB.Where("code = ?", "privacy").Find(&page)
+	data["md"] = parseMD(page.Text)
 	data["messages"] = flash.Get(w, r)
-
 	return render(w, data, "privacy/index.html")
 }
